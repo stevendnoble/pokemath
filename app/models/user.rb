d45c25143f9a6d6ac1c58d6d365d1515c5a08575
alanlee35 <alanblee12@gmail.com>
2016-01-11 15:53:17 -0800

    fixed assosications

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many :pokemons, through: :caught_pokemon
-  
-end
+  has_many :caught_pokemons, dependent: :destroy
+  has_many :pokemons, through: :caught_pokemons
+
+end 
