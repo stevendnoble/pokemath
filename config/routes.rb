fe5fea764beac12f356e9d3d1a7cce004dd6e9d1
alanlee35 <alanblee12@gmail.com>
2016-01-11 19:52:01 -0800

    passing map object to the site controller to save the session variable to that object

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   get '/login' => "sessions#new"
   post '/login' => "sessions#create" 
   get '/logout' => "sessions#destroy"
-
+  post '/savemap' => "site#mapstate"
 end
 
