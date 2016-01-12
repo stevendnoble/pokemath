7fa6b471b37f47c10e492ba5958c91bbe7bb64f7
alanlee35 <alanblee12@gmail.com>
2016-01-12 08:18:51 -0800

    comments for code

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,6 +4,8 @@ class SiteController < ApplicationController
   # before_filter :authorize
   
   def index
+    #session variable sets the key :map_state to the data passed through from ajax call
+    #'should' be working, theres a 200 response with the data as string in the logs
     session[:map_state] = params[:data_value]
     @rows = 6
     @columns = 12
