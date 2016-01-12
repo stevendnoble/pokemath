c96e4fa6770ab3000504aa7d96965f60df29ffd9
stevennoble78 <stevennoble78@hotmail.com>
2016-01-12 13:34:46 -0800

    move on map refactored, add initiate battle screen

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,16 @@ Rails.application.routes.draw do
   resources :users, except: [:index, :edit, :update]
   resources :caught_pokemons, only: [:create, :show, :destroy]
   resources :site, only: [:index, :about, :map]
-
-  get "/map" => "site#map"
-
-  get "/signup" => "users#new"
-  post "/users" => "users#create"
-
-
-  get "/login", to: "sessions#new"
-  get "/logout", to: "sessions#destroy"
   resources :sessions, only: [:create]
 
-  post '/mapstate' => "mapstates#create"
-  get '/mapstate' => "mapstates#index"
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+
+  get "/map", to: "site#map"
+  get "/battle", to: "pokemon#show"
+  post "/mapstate", to: "mapstates#create"
+  get "/mapstate", to: "mapstates#index"
 
 end
 
