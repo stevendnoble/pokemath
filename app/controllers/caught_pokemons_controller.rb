2eb363b468b0d448df44144a73c9cd02a4710875
alanlee35 <alanblee12@gmail.com>
2016-01-11 16:59:20 -0800

    form to save caught pokemon working

--- app/controllers/caught_pokemons_controller.rb
+++ app/controllers/caught_pokemons_controller.rb
@@ -1,8 +1,8 @@
-class CaughtPokemonController < ApplicationController
+class CaughtPokemonsController < ApplicationController
   def create
     @caught_pokemon = CaughtPokemon.new(caught_pokemon_params)
     if @caught_pokemon.save
-      redirect_to caught_pokemon_path(@caught_pokemon)
+      redirect_to user_path(current_user)
     else
       flash[:error] = @caught_pokemon.errors.full_messages.join(', ')
       redirect_to root_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user
   def new
     if current_user
       redirect_to user_path(current_user)
@@ -30,6 +30,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:username, :password, :password_confirmation)
