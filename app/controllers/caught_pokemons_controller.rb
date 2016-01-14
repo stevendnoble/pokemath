58e7b6b6a66d41a8f88bf0fb99eafc30cb89b7d0
alanlee35 <alanblee12@gmail.com>
2016-01-14 12:54:27 -0800

    changing up the about_page styles and added the option to release pokemon

--- app/controllers/caught_pokemons_controller.rb
+++ app/controllers/caught_pokemons_controller.rb
@@ -1,5 +1,6 @@
 class CaughtPokemonsController < ApplicationController
   before_action :authorize
+  before_action :set_pokemon, only:[:show,:destroy]
   
   def create
     @caught_pokemon = CaughtPokemon.new(caught_pokemon_params)
@@ -12,16 +13,26 @@ class CaughtPokemonsController < ApplicationController
   end
 
   def show
-    @caught_pokemon = CaughtPokemon.find(params[:id]);
     @pokemon = Pokemon.find_by_id(@caught_pokemon.pokemon_id);
 
   end
 
   def destroy
+    if @caught_pokemon.user_id == current_user.id
+      @caught_pokemon.destroy
+      flash[:notice] = "You let your best friend go."
+      redirect_to user_path(current_user)
+    else
+      redirect_to user_path(current_user)
+      flash[:error] = "Not your pokemon!"
+    end
   end
 
   private
 
+  def set_pokemon
+    @caught_pokemon = CaughtPokemon.find(params[:id]);
+  end
   def caught_pokemon_params
     params.require(:caught_pokemon).permit(:name, :user_id, :pokemon_id)
   end
