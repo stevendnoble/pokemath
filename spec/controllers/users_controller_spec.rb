a0a57946c0e30c4cd48ff7717b6f4e4e21fb71ef
TheEddyChang <ehchang58@yahoo.com>
2016-01-14 17:48:50 -0800

    heroku fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
+require 'ffaker'
 
 RSpec.describe UsersController, type: :controller do
 
+
+    before do
+      user_params = Hash.new
+      user_params[:username] = FFaker::Name.name
+      user_params[:password] = FFaker::Lorem.words(2).join
+      user_params[:password_confirmation] = user_params[:password]
+      @current_user = User.create(user_params)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
+    end
+
+
+
+    describe "GET #new" do
+      it "should assign @user" do
+        get :new
+        expect(assigns(@current_user)).to be_instance_of(:user)
+      end
+
+      it "should render the :new view" do
+        get :new
+        expect(response).to redirect_to(user_path(@current_user))
+      end
+    end
 end
+
+    
+
+
+
+
+
